642efdca150b1abe70db6f338019c3140e8e10f4
Rahul Mestri <rahulmestri239@gmail.com>
2026-01-26 02:03:25 +0530

    basic api testing validations

---
+++ tests/basicapitests.spec.js
@@ -0,0 +1,35 @@
+import {test, expect} from '@playwright/test';
+
+//api testing
+
+test ('loginrequest', async({request})=>
+{
+
+const response = await request.get('https://rahulshettyacademy.com/client/');
+
+expect(response.status()).toBe(200);
+
+
+    }
+    
+    
+    )
+
+test ('postrequestlogin', async({request})=>{
+
+const responseBody = await request.post('https://rahulshettyacademy.com/api/ecom/auth/login' ,  {
+
+data : {
+  userEmail: "rahulmestri239@gmail.com",
+  userPassword: "Beautiful@678#"
+}
+
+})
+
+expect(responseBody.status()).toBe(200);
+const body = await responseBody.json();
+console.log('Response Body:', body);
+console.log('Token:', body.token);
+expect(body.message).toBe('Login Successfully');
+
+})
